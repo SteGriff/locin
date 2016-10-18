da8317caa6cb52ce8b427b35501ca857a7c9e6e6
SteGriff <github@stegriff.co.uk>
2016-10-18 15:55:53 +0100

    init;

---
+++ locin/Options.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace locin
+{
+    class Options
+    {
+        [Option('f', "filters", DefaultValue = "*.*", HelpText = "File name/type filters E.g. *.cs;*.xml")]
+        public string filters { get; set; }
+
+        [Option('r', "recursive", DefaultValue = true, HelpText = "Get files recursively rather than using the top directory only.")]
+        public bool recursive { get; set; }
+
+        [Option('e', "include-empty", DefaultValue = false, HelpText = "Include empty lines in count")]
+        public bool includeEmpty { get; set; }
+
+        [Option('o', "outfile", DefaultValue = null, HelpText = "Output results to this file path instead of printing to console")]
+        public string outfile { get; set; }
+
+    }
+}
---
+++ locin/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace locin
+{
+    class Program
+    {
+        static Options options = new Options();
+        static List<string> outputFileLines;
+
+        static void Main(string[] args)
+        {
+            outputFileLines = new List<string>();
+
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Environment.Exit(0);
+            }
+
+            var filters = new List<string> { options.filters };
+            if (options.filters.Contains(";"))
+            {
+                filters = options.filters.Split(new[] { ';' }).ToList();
+            }
+
+            //Prepare file filters and options,
+            // figure out which files to read
+            var filesToRead = new List<string>();
+            var searchOption = options.recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            string path = Environment.CurrentDirectory;
+            foreach (var filter in filters)
+            {
+                try
+                {
+                    var files = Directory.EnumerateFiles(path, filter, searchOption);
+                    filesToRead.AddRange(files);
+                }
+                catch (Exception)
+                {
+                    Output("Invalid filter, {0}", ConsoleColor.Red, filter);
+                }
+
+            }
+
+            //Count lines in the target files
+            var linesByFile = new Dictionary<string, int>();
+            int totalLines = 0;
+            int pathLength = path.Length;
+            foreach (var file in filesToRead)
+            {
+                int lines = 0;
+                if (options.includeEmpty)
+                {
+                    lines = File.ReadLines(file).ToList().Count;
+                }
+                else
+                {
+                    lines = File.ReadLines(file).Count(l => !string.IsNullOrWhiteSpace(l));
+                }
+
+                linesByFile.Add(file.Substring(pathLength), lines);
+                totalLines += lines;
+            }
+
+            //Output the results
+            Output("locin - {0}", path);
+            Output("Total lines: {0}", totalLines);
+            Output("Breakdown:");
+            Output("==========");
+            foreach (var file in linesByFile.OrderByDescending(f => f.Value))
+            {
+                Output("{0}\t{1}", file.Value, file.Key);
+            }
+
+            Output("==========");
+            Output("Total lines: {0}", totalLines);
+
+            if (options.outfile != null)
+            {
+                try
+                {
+                    File.WriteAllLines(options.outfile, outputFileLines);
+                }
+                catch (Exception)
+                {
+                    //Strict fallback to console for error message
+                    Console.WriteLine("Couldn't write to output file.");
+                }
+
+            }
+
+        }
+
+        private static void Output(string format, ConsoleColor colour, params object[] args)
+        {
+            if (options.outfile == null)
+            {
+                Console.ForegroundColor = colour;
+                Console.WriteLine(format, (object[])args);
+                Console.ResetColor();
+            }
+            else
+            {
+                outputFileLines.Add(string.Format(format, (object[])args));
+            }
+        }
+
+        private static void Output(string format, params object[] args)
+        {
+            if (options.outfile == null)
+            {
+                Console.WriteLine(format, (object[])args);
+            }
+            else
+            {
+                outputFileLines.Add(string.Format(format, (object[])args));
+            }
+        }
+
+        private static void Output(string text)
+        {
+            if (options.outfile == null)
+            {
+                Console.WriteLine(text);
+            }
+            else
+            {
+                outputFileLines.Add(text);
+            }
+        }
+
+        private static void Output(string text, ConsoleColor colour)
+        {
+            if (options.outfile == null)
+            {
+                Console.ForegroundColor = colour;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+            else
+            {
+                outputFileLines.Add(text);
+            }
+        }
+
+    }
+}
